cc7708c93b88aa131d0a4c5fbeea6dedf7116af8
annikcwq <129215309+annikcwq@users.noreply.github.com>
2023-09-11 12:46:46 -0400

    refactored to extract helper method

--- src/FizzBuzz.java
+++ src/FizzBuzz.java
@@ -4,31 +4,64 @@
 class FizzBuzz {
 
     public static void main(String[] args) {
+//        using a for loop
+//        for (int i = 1; i < 100; i++) {
+//
+//            // Find out which numbers divide i.
+//            boolean divisibleBy3 = i % 3 == 0;
+//            boolean divisibleBy5 = i % 5 == 0;
+//
+//            // Print our appropriate result.
+//            if (divisibleBy3 && divisibleBy5) {
+//
+//                System.out.println("Fizz Buzz");
+//
+//            } else if (divisibleBy3) {
+//
+//                System.out.println("Fizz");
+//
+//            } else if (divisibleBy5) {
+//
+//                System.out.println("Buzz");
+//
+//            } else {
+//
+//                System.out.println(i);
+//
+        // using a while loop
+          int i = 1;
+          while (i < 100) {
 
-        for (int i = 1; i < 100; i++) {
+              i = doFizzBuzz(i);
 
-            // Find out which numbers divide i.
-            boolean divisibleBy3 = i % 3 == 0;
-            boolean divisibleBy5 = i % 5 == 0;
 
-            // Print our appropriate result.
-            if (divisibleBy3 && divisibleBy5) {
+          }
+    }
 
-                System.out.println("Fizz Buzz");
+    private static int doFizzBuzz(int i) {
+        // Find out which numbers divide i.
+        boolean divisibleBy3 = i % 3 == 0;
+        boolean divisibleBy5 = i % 5 == 0;
 
-            } else if (divisibleBy3) {
+        // Print our appropriate result.
+        if (divisibleBy3 && divisibleBy5) {
 
-                System.out.println("Fizz");
+            System.out.println("Fizz Buzz");
 
-            } else if (divisibleBy5) {
+        } else if (divisibleBy3) {
 
-                System.out.println("Buzz");
+            System.out.println("Fizz");
 
-            } else {
+        } else if (divisibleBy5) {
 
-                System.out.println(i);
+            System.out.println("Buzz");
 
-            }
+        } else {
+
+            System.out.println(i);
         }
+
+        i++;
+        return i;
     }
 }
