a4daa002625ca23449707b463d74e487a4f0372d
annikcwq <129215309+annikcwq@users.noreply.github.com>
2023-09-12 15:52:53 -0400

    Added comments in main method

--- src/Reduce.java
+++ src/Reduce.java
@@ -1,2 +1,32 @@
-package PACKAGE_NAME;public class Reduce {
+public class Reduce {
+
+    public static void main(String[] args) {
+
+        int n = 100;
+        int count = 0;
+
+        while (n > 0) {
+
+            // Check if 2 divides n.
+            if (n % 2 == 0) {
+
+                // Divide n by 2 when n is even.
+                n = n / 2;
+
+            } else {
+
+                // Decrease n by 1 when n is odd.
+                n--;
+
+            }
+
+            // Increase count of steps by 1.
+            count++;
+
+        }
+
+        // Print result.
+        System.out.println("It takes " + count + " steps to reach 0 from 100.");
+
+    }
 }
